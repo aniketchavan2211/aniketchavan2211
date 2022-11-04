bf8397a73484656715d7b622b4ef8dc44bc1f158
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-04 22:40:42 +0530

    Illustrate Different Set Operations

---
+++ Python/Projects/Illustrate Different Set Operations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+A = {1, 2, 3, 6, 8, 9}
+B = {3, 4, 5, 6, 7, 8}
+
+print ("The Union is ", A | B)
+
+print ("The Intersection is ", A & B)
+
+print ("The Difference is ", A - B)
+
+print ("The Symmetric Differenceis ", A ^ B)
