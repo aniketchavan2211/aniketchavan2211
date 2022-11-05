2c422b7e766fe235dfd34227eb312148163d5efd
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-05 23:36:06 +0530

    Accessing Index of List Using For loops

---
+++ Python/Projects/Accessing Index of List Using For loops.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+list1 = [12, 54, 443, 636, 73, 556]
+
+# for index, value in enumerate(list1, start=1):
+#   print(index,':', value)
+
+for index in range (len(list1)):
+  value = list1[index]
+  print (index, ':', value)
