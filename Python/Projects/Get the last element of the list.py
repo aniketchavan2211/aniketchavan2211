5f089d4d6255ac66e49bd9626b4b1d16bd21967c
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-09 00:26:34 +0530

    Adding Get the last element of the list Python Projects

---
+++ Python/Projects/Get the last element of the list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+Avengers = ['Iron Man', 'Thor', 'Hulk', 'Black Widow', 'Captain America', 'Hawkeye']
+
+print(Avengers[-1])
+
+# This will print last element Hawkeye if change then also print last element.
