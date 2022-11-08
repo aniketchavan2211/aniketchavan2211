343ff9acd3020716e50197b599a56e524b526625
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-08 19:57:49 +0530

    ADDING slicing lists

---
+++ Python/Projects/slicing list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+list = ['Aatrox', 'Samauel', 'Ishita', 'Shruti', 'Samyak', 'Aryan']
+
+print (list)
+
+print(list[2:]) # start
+
+print(list[:4]) # end
+
+print(list[2:4]) # specify start and end index
+
+print(list[::1]) # step/jump/gap
+
+print(list[-1]) # last element
+
+print(list[-6:-3]) # first 3 element
+
+print(list[::-1]) # reverse order
