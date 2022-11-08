a9e1658ebd963ee891251c95b9850fa69bb7df62
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-08 20:17:51 +0530

    ADDING iterate over dictionaies using for loop IN Python

---
+++ Python/Projects/iterate over dictionaies using for loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+dict1 = {'Vijay':'Alhat', 'Omkar':'Kharie', 'Abhishek':'Giri'}
+
+# for key, value in dict1.items():
+#   print(key, value)
+
+# for key in dict1:
+#   print (key, dict1[key])
+
+for key in dict1.keys():
+  print(key)
+
+for value in dict1.values():
+  print(value)
