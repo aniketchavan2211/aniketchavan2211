a6676d6caa6e88e74c408923d24b91cbeb6717fc
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-08 22:24:47 +0530

    Adding Copy Content from file to another filePython Projects

---
+++ Python/Projects/Copy Content from one file to another file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from shutil import copyfile
+
+# Copy Content From file to Another file.
+
+copyfile('from file path', 'to file path')
+# copyfile('/home/file1.txt', '/root/home/file2.txt')
+
+# Content of file1.txt and file2.txt are same.
