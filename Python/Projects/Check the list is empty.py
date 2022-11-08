6c070eb67fde58293dc2f1c1c369e1b24a4ea717
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-08 20:45:33 +0530

    Adding Check the list is empty Python Projects

---
+++ Python/Projects/Check the list is empty.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+MyList = []
+
+#if not MyList:
+#  print('The list is EMPTY')
+
+#if len(MyList) == 0:
+#  print ('The list is EMPTY')
+
+if MyList == []:
+  print ('The list is EMPTY')
