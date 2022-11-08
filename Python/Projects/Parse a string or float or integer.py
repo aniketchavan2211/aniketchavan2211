69b1bb1744607669ad9aecff69dd97eb4fd4907e
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-08 23:56:23 +0530

    Adding Parse a string or float or integer Python Projects

---
+++ Python/Projects/Parse a string or float or integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+string = input('Enter: ')
+print(type(string))
+
+int_num = int(string) # Convert to Integer
+print('')
+print(int_num)
+print(type(int_num))
+
+print('')
+float_num = float(string) # Convert to floating-point number or decimal number
+
+print(float_num)
+print(type(float_num))
