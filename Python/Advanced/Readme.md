4df7154a6c57ce9b94597c6d587a159a4b52ab0f
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-11-19 20:23:33 +0530

    adding Advanced Python

---
+++ Python/Advanced/Readme.md
@@ -0,0 +1,24 @@
+##  Magic Methods
+
+**Dundar -> __abc__**
+
+### Object and Classes
+
+```py
+class Person:
+
+  def __init__(self,  name, age):
+    self.name = name
+    self.age = age
+
+  def __del__(self):
+    print("Object DELETE/Deconstructed ")
+
+  def 
+
+# Object p
+p = Person("Mike", 34)
+
+
+
+```
---
+++ Python/Advanced/magic-methods.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+class Vector():
+
+  def __init__(self, x ,y):
+    self.x = x
+    self.y = y
+
+  def __add__(self, other):
+    return Vector(self.x + other.x, self.y + other.y)
+
+  def __repr__(self):
+    return f"X: {self.x}, Y: {self.y}"
+
+  def __len(self):
+    return 10
+
+  def __call_(self):
+    print("Hello, This is a called")
+
+v1 = vector(10, 20)
+v2 = Vector(50, 60)
+
+# v3 = v1 + v2 is not working so used magic methods
+
+print(v3.x)
+# 60
+print(v3.y)
+# 80
+
+print(v3)
+
+print(len(v3)) # 10
