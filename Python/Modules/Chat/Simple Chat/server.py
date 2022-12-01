c8c69885aa126d6898c395d3b8c333a1b76573e4
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-12-01 17:42:29 +0530

    adding simple chat script server.py and client.py

---
+++ Python/Modules/Chat/Simple Chat/server.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 8000))
+
+server.listen()
+
+client, addr = server.accept()
+
+done = False
+
+while not done:
+  msg = client.recv(1024).decode('utf-8')
+  if msg == 'quit':
+    done = True
+  else:
+    print(msg)
+  client.send(input("Message: ").encode('utf-8'))
