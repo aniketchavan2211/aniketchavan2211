4cc7ff28fe9a67899c62fc1e4d4d041cd900727c
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-12-19 10:32:56 +0530

    moving  if ... and arguments parsing

--- Python/Modules/arguments parsing/main_v1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def myfunc(*args, **kwargs):
-  print(args[0])
-  print(args[1])
-  print(args[2])
-  print(kwargs['KEYWD'])
-
-myfunc('a', 'b', 'c', KEYWD=1)
--- Python/Modules/if __name__ == __main__/first_module.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-  print('Run from First module')
-
-if __name__ == '__main__':
-  main()
-else:
-  print('imported first module run')
