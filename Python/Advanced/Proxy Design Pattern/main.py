523119263d3c1b251ed431a7e78b75e8c0280f9b
aniketchavan2211 <aniketchavan2211@gmail.com>
2022-12-23 20:00:26 +0530

    adding proxy design pattern

---
+++ Python/Advanced/Proxy Design Pattern/main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from abc import ABCMeta, abstractstaticmethod
+
+class IPerson(metaclass=ABCMeta):
+
+  @abstractstaticmethod
+  def person_method():
+    """ Interface Method  """
+
+class Person(IPerson):
+
+  def person_method(self):
+    print("I am a person")
+
+class ProxyPerson(IPerson):
+
+  def __init__(self):
+    self.person = Person()
+
+  def person_method(self):
+    print("I am the proxy functionality")
+    self.person.person_method()
+
+p1 = Person()
+p1.person_method()
+
+p2 = ProxyPerson()
+p2.person_method()
+
