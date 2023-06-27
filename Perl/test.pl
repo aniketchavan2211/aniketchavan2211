93bb4a86b813dc926cc8a3cfdcf9aec8cc1cc945
aniketchavan2211 <aniketchavan2211@gmail.com>
2023-06-27 17:56:55 +0530

    modified test.pl

--- Perl/test.pl
+++ Perl/test.pl
@@ -1,10 +1,2 @@
 #!/usr/bin/perl
 print("Hello, Perl\n\n");
-
-# subroutine
-sub funcName(value) {
-  print("$value")
-  #return ($value); # when arguments/parameters are pasing through function/subroutine
-}
-
-&funcName("5"); # (specify Arguments in round brackets)
