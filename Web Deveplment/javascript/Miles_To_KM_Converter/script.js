13b096f9c842ea189a868b6ea921119df234448b
aniketchavan2211 <aniketchavan2211@gmail.com>
2023-09-25 18:56:36 +0530

    modified playlist-app && miles-to-km-converter

--- Web Deveplment/javascript/Miles_To_KM_Converter/script.js
+++ Web Deveplment/javascript/Miles_To_KM_Converter/script.js
@@ -1,4 +1,5 @@
 var miles = "500";
 var kilometers = miles * 1.609344;
+
 console.log(kilometers);
 
--- Web Deveplment/javascript/Playlist_App/script.js
+++ Web Deveplment/javascript/Playlist_App/script.js
@@ -1,7 +1,10 @@
 var song = "Don't stop me now";
 var included = true;
+
 console.log("New Entry: " + song);
+
 if (included) {
  console.log("Already included!");
 }
+
 console.log("Save and continue");
