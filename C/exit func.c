4a6558be7de2fb8db248f82fe2f85a0a068eecc4
aniketchavan2211 <aniketchavan2211@gmail.com>
2023-09-26 14:40:57 +0530

    Adding exit func

---
+++ C/exit func.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\nStarting of the Program.\n");
+    printf("\nExiting of the Program.\n");
+    exit(0);
+    printf("End of the Program.\n");
+    return 0;
+}
