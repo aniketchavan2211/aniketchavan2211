dc5c049d3efa9414f52f994742428e6e1943a449
aniketchavan2211 <aniketchavan2211@gmail.com>
2023-10-29 17:34:34 +0530

    Modified website/static/index.js file & base.html and login.html in website/templates

--- Web Deveplment/python/flask/projects/Notes/website/static/index.js
+++ Web Deveplment/python/flask/projects/Notes/website/static/index.js
@@ -1,1 +1,8 @@
-document.write("Hell0");
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
