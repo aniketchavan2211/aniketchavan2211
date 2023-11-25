4a2532eef2e3a76f3f85570a93615b8667b8d2e9
aniketchavan2211 <aniketchavan2211@gmail.com>
2023-11-25 15:16:21 +0530

    Adding main.py

---
+++ Python/Styling/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+"""
+    Project Name: My Data (Copyright 2023)
+    Description: My personal data
+    Author: Aniket Chavan
+    Date: 21 Nov 2023
+
+"""
+
+
+
+
+import Hi Hackers!!!
+
+def Bio():
+    name         : str = "Aniket Chavan"
+    aka          : list = ["$h4d0w Knight", "De4dy Be4st"]
+    status       : list = ["Student", "Penetration Tester"]
+    team         : str = "Shadow Knight's"
+    designation  : str = "Leader"
+    place        : str = "Pune, India"
+    work area    : str = "Cybersecurity"
+
+def Stack():
+    langauges    : list = ["Python", "C/Cpp", "HTML/CSS/JS"]
+    scripts      : list = ["Bash", "Batch", "Powershell"]
+    databases    : list = ["SQLite3"]
+
+def Social():
+    github       : str = "aniketchavan2211"
+    tryhackme    : str = "AnonymousOnes"
+    hackthebox   : str = "AnonymousOnes"
+    picoCTF      : str = "aniketchavan2211"
+    twitter      : str = "Aniket86002211"
+    instagram    : str = "aniket_chavan_2211"
+    linkedin     : str = "Aniket Chavan"
